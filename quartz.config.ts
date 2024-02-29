1486ea6a890bc4f5bc1fde6a2719dc9ffd7bef0f
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2024-02-29 00:18:28 -0300

    add vercel support

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "notas.vicente.cl",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
