0a64101a7599b308091eea343c0beff39e941a94
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2024-03-04 10:56:27 -0300

    add notes

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "📝 Notas de Vicho",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
