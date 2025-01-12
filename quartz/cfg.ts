9f6f80d67cc3daa54cfad5df6b6c5bde71594a9c
Vicente Matus <38754555+vicentematus@users.noreply.github.com>
2025-01-11 23:11:01 -0300

    feat: modify analyitcs to try see if posthog works

--- quartz/cfg.ts
+++ quartz/cfg.ts
@@ -19,6 +19,11 @@ export type Analytics =
       websiteId: string
       host?: string
     }
+  | {
+      provider: "posthog"
+      apiKey: string
+      host?: string
+    }
 
 export interface GlobalConfiguration {
   pageTitle: string
